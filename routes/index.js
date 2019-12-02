8d724867155f262aa4a9a3203edd321db2dd55bf
HOres20 <38805512+HOres20@users.noreply.github.com>
2019-12-02 19:52:31 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
 // Dashboard
-router.get('/index.html', ensureAuthenticated, (req, res) =>
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
   res.render('dashboard', {
     user: req.user
   })
